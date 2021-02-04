26607c253f9e2722229c305687448930a2b759bb
poshih <poshih@gmail.com>
2021-02-04 02:42:50 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -13,6 +13,6 @@ trap 'cleanup' SIGTERM
 sudo nvidia-smi -pm 1
 sudo nvidia-smi -pl 110
 
-ethminer --report-hashrate -U -P stratum1+tcp://0xA494F07b0Cd0315903d2461c10AED0246477A759.$(whoami)@us2.ethermine.org:4444 &
+ethminer --report-hashrate -U -P stratum1+tcp://0xA494F07b0Cd0315903d2461c10AED0246477A759.$(cat /etc/hostname)@us2.ethermine.org:4444 &
 
 while true; do :; done
