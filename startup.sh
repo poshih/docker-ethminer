1eaa4c587989a8529b08a03abdea5498182bcc78
poshih <poshih@gmail.com>
2021-02-07 15:52:25 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -15,4 +15,7 @@ sudo nvidia-smi -pl 110
 
 ethminer --report-hashrate -U -P stratum1+tcp://0xA494F07b0Cd0315903d2461c10AED0246477A759.docker_$(cat /etc/hostname)@us2.ethermine.org:4444 &
 
-while true; do :; done
+while true;
+do 
+sleep 2
+done
