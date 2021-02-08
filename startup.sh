e38f6844310542f0fa194670868d933d786ddf7b
poshih <poshih@gmail.com>
2021-02-07 23:59:10 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -13,7 +13,7 @@ trap 'cleanup' SIGTERM
 sudo nvidia-smi -pm 1
 sudo nvidia-smi -pl 110
 
-ethminer --report-hashrate -U -P stratum1+tcp://0xA494F07b0Cd0315903d2461c10AED0246477A759.docker_$(cat /etc/hostname)@us2.ethermine.org:4444 &
+ethminer --report-hashrate -U -P stratum1+tcp://0xA494F07b0Cd0315903d2461c10AED0246477A759.docker_$(cat /etc/hostname)@eth-us-west.flexpool.io:4444 &
 
 while true;
 do 
