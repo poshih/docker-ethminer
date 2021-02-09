6a7c0b8c0a9606e645a09d4edaac3a30f4566553
poshih <poshih@gmail.com>
2021-02-08 21:37:47 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -13,7 +13,7 @@ trap 'cleanup' SIGTERM
 sudo nvidia-smi -pm 1
 sudo nvidia-smi -pl 110
 
-until ethminer --report-hashrate -U -P stratum1+tcp://0xA494F07b0Cd0315903d2461c10AED0246477A759.docker_$thefounder@eth-us-west.flexpool.io:4444
+until ethminer --report-hashrate -U -P stratum1+tcp://0xA494F07b0Cd0315903d2461c10AED0246477A759.docker_thefounder@eth-us-west.flexpool.io:4444
 do 
 echo "ethminer crashed with exit code $?.  Respawning.." >&2
 sleep 2
